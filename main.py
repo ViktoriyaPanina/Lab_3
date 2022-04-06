3c71fa7458954400ebd91537d272da5e4ebf4517
ViktoriyaPanina <92367114+ViktoriyaPanina@users.noreply.github.com>
2022-04-06 22:07:39 +0300

    Добавление сортировки слиянием

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 from random import randint
 
+# Adding merge sort
+def merge(left, right, compare):
+    result = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if compare(left[i], right[j]):
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+    return result
+
 def insertion_sort(arr):
     for i in range(1, len(arr)):
         key = arr[i]
